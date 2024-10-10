ca3c22c9db1d12952e6607a3047e8a4720a4b418
Kreida Anton <kreida.anton@yandex.ru>
2024-10-10 22:44:20 +0500

    home-page | fix

--- src/widgets/sidebar/ui/navigate-menu/nav-item.tsx
+++ src/widgets/sidebar/ui/navigate-menu/nav-item.tsx
@@ -15,8 +15,8 @@ const NavItem: FC<INavItemProps> = ({ navItem, isActiveLink }) => (
   <li
     className={ twClassNames(`relative text-carbon text-sm py-4 font-futura-pt
       font-medium uppercase hover:text-orochimaru transition-[color]`,{
-      [`before:absolute before:w-[3px] before:left-[-10px]
-      before:top-0 before:h-[18px] before:bg-orochimaru`]: isActiveLink
+      [`before:absolute before:w-[3px] before:left-[-10px] before:translate-y-[-50%]
+      before:top-[50%] before:h-[18px] before:bg-orochimaru`]: isActiveLink
     }) }
     key={ navItem.id }
   >
