fd636a2b87c86f1b8185a892c039be32eccc4987
Kreida Anton <kreida.anton@yandex.ru>
2024-10-13 14:59:53 +0500

    session | fix schema feedback

--- src/features/form-feedback/model/schemas/schema-feedback.ts
+++ src/features/form-feedback/model/schemas/schema-feedback.ts
@@ -4,13 +4,14 @@ import { z } from "zod";
 
 const phoneRegex = /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/gi;
 
-export const schemaFeedback = z.object({
+export const FeedbackSchema = z.object({
   name: z.string({ required_error: "Укажите свое имя!" }).min(1, "Укажите свое имя!"),
-  phone: z.string({ required_error: "Оставьте свой телефон!" }).min(1, "Оставьте свой телефон!").regex(phoneRegex, "Не правильный формат номера!"),
+  phone: z.string({ required_error: "Оставьте свой телефон!" }).min(1, "Оставьте свой телефон!")
+    .regex(phoneRegex, "Не правильный формат номера!"),
   message: z.string().optional(),
 }).refine((data) => validator.isMobilePhone(data.phone.replace(/[\s()-]/gi,""), ["ru-RU", "kk-KZ", "uk-UA", "be-BY"]), {
   message: "Не правильный формат номера",
   path: ["clientPhone"],
 });
 
-export type TSchemaFeedbackDto = z.infer<typeof schemaFeedback>;
+export type TSchemaFeedbackDto = z.infer<typeof FeedbackSchema>;
