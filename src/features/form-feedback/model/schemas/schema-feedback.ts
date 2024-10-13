4e0f6f7dc778ef862576771836271d3960d9d0b9
Kreida Anton <kreida.anton@yandex.ru>
2024-10-13 14:55:46 +0500

    session | add schema for form-feedback

---
+++ src/features/form-feedback/model/schemas/schema-feedback.ts
@@ -0,0 +1,16 @@
+import validator from "validator";
+import { z } from "zod";
+
+
+const phoneRegex = /^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$/;
+
+export const schemaFeedback = z.object({
+  name: z.string({ required_error: "Укажите свое имя!" }).min(1, "Укажите свое имя!"),
+  phone: z.string({ required_error: "Оставьте свой телефон!" }).min(1, "Оставьте свой телефон!").regex(phoneRegex, "Не правильный формат номера!"),
+  message: z.string().optional(),
+}).refine((data) => validator.isMobilePhone(data.phone.replace(/[\s()-]/g,""), ["ru-RU", "kk-KZ", "uk-UA", "be-BY"]), {
+  message: "Не правильный формат номера",
+  path: ["clientPhone"],
+});
+
+export type TSchemaFeedbackDto = z.infer<typeof schemaFeedback>;
