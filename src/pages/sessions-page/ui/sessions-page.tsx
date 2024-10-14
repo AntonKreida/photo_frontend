f74ad5ee12761cb0ce7ae43bc0f0660d56d73a69
Kreida Anton <kreida.anton@yandex.ru>
2024-10-15 02:09:04 +0500

    session | add template style for grid

--- src/pages/sessions-page/ui/sessions-page.tsx
+++ src/pages/sessions-page/ui/sessions-page.tsx
@@ -3,6 +3,40 @@ import { ArrowRightIcon } from "@heroicons/react/24/solid";
 import { FormContact } from "@features/";
 import { HeaderPage } from "@shared/";
 
+import { SessionCard } from "./session-card";
+import mockImage from "../mock/mock.jpg";
+
+
+const listImage = [
+  {
+    href: mockImage,
+    title: "Алена"
+  },
+  {
+    href: mockImage,
+    title: "Алена"
+  },
+  {
+    href: mockImage,
+    title: "Алена"
+  },
+  {
+    href: mockImage,
+    title: "Алена"
+  },
+  {
+    href: mockImage,
+    title: "Алена"
+  },
+  {
+    href: mockImage,
+    title: "Алена"
+  },
+  {
+    href: mockImage,
+    title: "Алена"
+  },
+];
 
 export const SessionsPage = () => (
   <div className="w-full">
@@ -16,6 +50,33 @@ export const SessionsPage = () => (
       subTitle="портретная съемка как зеркало души"
       title="Личные фотосессии"
     />
+
+    <div className={ `grid h-fit grid-cols-12 gap-5 mt-14
+        [&>*:nth-child(12n-2)]:col-span-8 [&>*:nth-child(12n-2)]:row-span-2
+        [&>*:nth-child(12n-7)]:col-span-8 [&>*:nth-child(12n-7)]:row-span-2
+        [&>*:nth-last-child(1):nth-child(12n-10)]:col-span-8
+        [&>*:nth-last-child(1):nth-child(12n-11)]:col-span-12
+        [&>*:nth-last-child(1):nth-child(12n-11)]:row-span-2
+        [&>*:nth-last-child(1):nth-child(12n-2)]:col-span-4
+        [&>*:nth-last-child(1):nth-child(12n-2)]:row-span-1
+        [&>*:nth-last-child(2):nth-child(12n-8)]:row-span-2
+        [&>*:nth-last-child(2):nth-child(12n-9)]:row-span-2
+        [&>*:nth-last-child(3):nth-child(12n-3)]:row-span-2
+        [&>*:nth-last-child(3):nth-child(12n-4)]:row-span-2
+        [&>*:nth-last-child(3):nth-child(12n-6)]:row-span-2
+        [&>*:nth-last-child(4):nth-child(12n-11)]:row-span-2
+        [&>*:nth-last-child(5):nth-child(12n-6)]:row-span-2
+        [&>*:nth-last-child(5):nth-child(12n-9)]:row-span-2
+        [&>*:nth-last-child(7):nth-child(12n-11)]:row-span-2` }
+    >
+      { listImage.map((image, index) => (
+        <SessionCard
+          href={ image.href }
+          key={ index }
+          titleCard={ image.title }
+        />
+      )) }
+    </div>
   </div>
 );
 
