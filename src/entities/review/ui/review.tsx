3c9188990eff1b3c7d56f815e7bef0671d54df90
Kreida Anton <kreida.anton@yandex.ru>
2024-11-08 13:00:10 +0500

    reviews | fix style

--- src/entities/review/ui/review.tsx
+++ src/entities/review/ui/review.tsx
@@ -1,9 +1,6 @@
-import { twClassNames } from "@shared/";
-
-
 export const Review = () => (
-  <div className="w-full grid grid-cols-2 min-h-[295px]">
-    <div className={ twClassNames("col-span-1 col-start-1") }>фцв</div>
-    <div className="col-span-1"></div>
+  <div className="w-full grid grid-cols-2 min-h-[295px] grid-rows-1">
+    <div className="col-span-1 col-start-1 row-start-1">фотка</div>
+    <div className="col-span-1 col-start-2 row-start-1">коммент</div>
   </div>
 );
--- src/widgets/reviews-feed/ui/reviews-feed.tsx
+++ src/widgets/reviews-feed/ui/reviews-feed.tsx
@@ -2,7 +2,9 @@ import { Review } from "@entities/";
 
 
 export const ReviewsFeed = () => (
-  <div className="mt-[76px] flex flex-col gap-[51px] [&>div:nth-child(even)>div]:col-start-2">
+  <div className="mt-[76px] flex flex-col gap-[51px] [&>div:nth-child(even)>div:nth-child(1)]:col-start-2
+  [&>div:nth-child(even)>div:nth-child(2)]:col-start-1"
+  >
     { Array.from({ length: 3 }).map((_, index) => (
       <Review key={ index } />
     )) }
