29c49949c153728e5d74ee22ec4afffed42defcb
Anton Kreida <kreida.anton@yandex.ru>
2025-03-30 19:29:06 +0500

    main | fix: fix main-page add card-link, remove slider

---
+++ src/pages/home-page/ui/card-link.tsx
@@ -0,0 +1,34 @@
+import { Link } from "next-transition-router";
+import { FC } from "react";
+
+import { AdapterImage } from "@shared/";
+
+import { ISlider } from "../lib";
+
+
+interface ICardLinkProps {
+    card: ISlider;
+}
+
+export const CardLink: FC<ICardLinkProps> = ({ card }) => (
+  <Link
+    className="flex flex-col gap-4 items-center sm:max-w-[300px]
+    h-fit col-span-3 w-full lg:col-span-4 group rounded-sm overflow-hidden"
+    href={ `${card.url}` }
+  >
+    <AdapterImage
+      alt={ card.title }
+      className="object-cover object-center"
+      classNameWrapper="sm:h-[310px] h-[500px] w-full w-full overflow-hidden shadow"
+      height={ card.sliderImage.height }
+      priority
+      quality={ 100 }
+      src={ `${ process.env.NEXT_PUBLIC_API_URL }${ card.sliderImage.url }` }
+      width={ card.sliderImage.width }
+    />
+
+    <span className="text-carbon text-2xl font-normal font-gabriela text-center group-hover:text-gray-300 transition-[color]">
+      { card.title }
+    </span>
+  </Link>
+);
--- src/pages/home-page/ui/home-page.tsx
+++ src/pages/home-page/ui/home-page.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { HeaderPage, LayoutContent } from "@shared/";
+import { HeaderPage, LayoutContent, twClassNames } from "@shared/";
 
-import { Slider } from "./slider";
 import { useHomePage } from "../model";
+import { CardLink } from "./card-link";
 
 
 export const HomePage = () => {
@@ -12,8 +12,25 @@ export const HomePage = () => {
   return (
     <div className="w-full flex flex-col h-full">
       <HeaderPage subTitle={ homePage?.subTitle } title={ homePage?.title } />
-      <LayoutContent className="flex-grow">
-        <Slider sliders={ homePage?.sliders ?? [] } />
+      <LayoutContent className={ twClassNames(`
+        grid lg:grid-cols-12 grid-rows-auto gap-[35px]
+        lg:[&>*:nth-child(5n-1)]:col-span-6 lg:[&>*:nth-child(5n)]:col-span-6
+        lg:[&>*:nth-child(5n-1)]:row-span-2 lg:[&>*:nth-child(5n)]:row-span-2
+        lg:[&>*:nth-child(5n-1)]:max-w-[485px] lg:[&>*:nth-child(5n)]:max-w-[485px]
+        lg:[&>*:nth-child(5n-1)>div]:max-w-[485px] lg:[&>*:nth-child(5n-1)>div]:h-[485px]
+        lg:[&>*:nth-child(5n)>div]:max-w-[485px] lg:[&>*:nth-child(5n)>div]:h-[485px]
+        sm:grid-cols-6 grid-cols-3`, {
+        "flex flex-col justify-center items-center h-full": homePage?.sliders.length === 0
+      }) }
+      >
+        { homePage?.sliders.length
+          ? homePage?.sliders.map((slider) => (
+            <CardLink
+              card={ slider }
+              key={ slider.documentId }
+            />
+          ))
+          : null }
       </LayoutContent>
     </div>
   );
